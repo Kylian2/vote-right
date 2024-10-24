17ccaa86cfa5bb226a5d87a5c80b8b684d6d1c0a
Esteban Rodrigues <esteban.rodrigues@universite-paris-saclay.fr>
2024-10-24 09:52:14 +0200

    DB : ajout membre

--- DB-scripts/insert-suffrage.sql
+++ DB-scripts/insert-suffrage.sql
@@ -1,3 +1,5 @@
-INSERT INTO SUFFRAGE (SUF_id_NB, SUF_libelle_VC) VALUES (1, 'Scrutin Majoritaire : Pour-Contre'),
-(2, 'Scrutin Majoritaire : Oui-Non'),
-(3, 'Scrutin proportionnel');
+INSERT INTO SUFFRAGE (SUF_id_NB, SUF_libelle_VC) VALUES (1, 'Pour / Contre'),
+(2, 'Oui / Non'),
+(3, 'Scrutin majoritaire à un tour'),
+(4, 'Scrutin majoritaire à deux tours'),
+(5, 'Scrutin proportionnel');
