4f5f2dcb86a3d1f2f760a5b6876a42ba22b218ed
Esteban <162978612+EstebanDebug@users.noreply.github.com>
2024-10-25 23:02:41 +0200

    fix : problème d'insertion dans motif

--- DB-scripts/insert-motif.sql
+++ DB-scripts/insert-motif.sql
@@ -5,6 +5,6 @@ INSERT INTO reason (RES_id_NB, RES_label_VC) VALUES
 (3, 'Maltraitance d''enfants'),
 (4, 'Violent ou abject'),
 (5, 'Sexuel'),
-(6, 'Offansant ou haineux'),
+(6, 'Offensant ou haineux'),
 (7, 'Spam ou prêtant à confusion'),
-(7, 'Acte dangereux et préjudiciable');
+(8, 'Acte dangereux et préjudiciable');
