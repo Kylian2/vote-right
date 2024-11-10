155355c24b701fcf1cf8f52bb154b3aa5ec041b8
Kylian2 <kylian.richard@gmail.com>
2024-11-10 14:15:08 +0100

    fix: retrait console.log

--- frontend/middleware/auth.ts
+++ frontend/middleware/auth.ts
@@ -1,7 +1,7 @@
 export default defineNuxtRouteMiddleware(async () => {
     //const config = useRuntimeConfig() //Pour utiliser les variables d'environnement, sera utile lorsque l'on mettra les routes de productions
     try {
-        const headers = useRequestHeaders(['cookie'])
+        const headers = useRequestHeaders(['cookie']);
         const response = await $fetch(`http://localhost:3333/auth/check`, { headers, credentials : 'include'});
         if (response !== true) { 
             return navigateTo('/login');
--- frontend/middleware/guest.ts
+++ frontend/middleware/guest.ts
@@ -1,10 +1,8 @@
 export default defineNuxtRouteMiddleware(async () => {
     //const config = useRuntimeConfig() //Pour utiliser les variables d'environnement, sera utile lorsque l'on mettra les routes de productions
     try {
-        const headers = useRequestHeaders(['cookie'])
-        console.log(headers)
+        const headers = useRequestHeaders(['cookie']);
         const response = await $fetch(`http://localhost:3333/auth/check`, { headers, credentials : 'include'});
-        console.log(response);
         if (response === true) { 
             return navigateTo('/home');
         }
