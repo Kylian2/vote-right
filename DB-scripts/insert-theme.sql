31e91b4856e393d7416130eea9a51e2d69d8f294
Esteban <162978612+EstebanDebug@users.noreply.github.com>
2024-11-12 19:49:28 +0100

    fix : renommage des fichiers en anglais

--- DB-scripts/insert-theme.sql
+++ DB-scripts/insert-theme.sql
@@ -1,5 +1,5 @@
 -- Insertions réalisées
-INSERT INTO `theme` (`THM_id_NB`, `THM_community_NB`, `THM_name_VC`, `THM_budget_NB`) VALUES
+INSERT INTO theme (THM_id_NB, THM_community_NB, THM_name_VC, THM_budget_NB) VALUES
 (1, 1, 'Voyages', 30000.00),
 (1, 2, 'Culture', 6000.00),
 (1, 3, 'Sortie', 2000.00),
