1d35813932c0b67f743742c953f15f467cb7acca
Kylian2 <kylian.richard@gmail.com>
2024-11-16 11:45:55 +0100

    feat: notifications summary
    Ajout des fonctions récupérants les données pour le résumé des notifications
    
    summary_comments: La liste des commentaires sur les propositions postées par l'utilisateur
    summary_proposals: La liste des nouvelles propositions de la semaines toutes communauté confondues
    summary_reactions_on_proposals: La somme des réactions j'aime j'aime pas pour chaque proposition postées par l'utilisateur
    summary_recations_on_comments: La somme des réactions [idem] pour chaque commentaire posté par l'utilisateur
    summary_started_votes: La liste des votes qui vont commencer pendant la semaine suivante
    summary_ended_votes: La liste des votes qui vont terminer la semaine suivante
    
    summary: regroupe les resultats de chacune des fonction si dessus en fonction des préférences de notifications de l'utilisateur
    
    Chacune des fonctions prends en paramètre un utilisateur et renvoie un JSON

---
+++ DB-scripts/functions/summary_reactions_on_proposals.sql
@@ -0,0 +1,67 @@
+DELIMITER //
+
+CREATE OR REPLACE FUNCTION summary_reactions_on_proposals(user_id INT)
+RETURNS TEXT
+NOT DETERMINISTIC
+BEGIN
+    DECLARE proposal_id INT;
+    DECLARE proposal_title VARCHAR(150);
+    DECLARE proposal_nblike SMALLINT;
+    DECLARE proposal_nbdislike SMALLINT;
+    DECLARE proposal_nblove SMALLINT;
+    DECLARE proposal_nbhate SMALLINT;
+
+    DECLARE one_proposal TEXT;
+    DECLARE list_proposal TEXT DEFAULT '[';
+    
+    DECLARE fin_cursor INT DEFAULT 0;
+    DECLARE first INT DEFAULT 1; -- Indicateur pour la première entrée sans virgule initiale
+
+    DECLARE proposals CURSOR FOR
+        SELECT pr.pro_id_nb, pr.pro_title_vc, nblove, nblike, nbdislike, nbhate
+        FROM proposal_total_reaction pr
+        INNER JOIN proposal p ON p.pro_id_nb = pr.pro_id_nb
+        WHERE pro_initiator_nb = user_id AND pr.pro_community_nb IN (SELECT mem_community_nb FROM member WHERE mem_user_nb = 92)
+        AND pro_status_nb = 'En cours';
+
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET fin_cursor = 1;
+
+    OPEN proposals;
+
+    fetchproposals: LOOP
+        FETCH proposals INTO proposal_id, proposal_title, proposal_nblove, proposal_nblike, proposal_nbdislike, proposal_nbhate;
+
+        IF fin_cursor THEN 
+            LEAVE fetchproposals; 
+        END IF;
+
+        -- Construction de l'objet JSON pour chaque proposition
+        SET one_proposal = CONCAT(
+            '{',
+            '"id": "', proposal_id, '",',
+            '"title": "', proposal_title, '",',
+            '"nblove": "', proposal_nblove, '",',
+            '"nblike": "', proposal_nblike, '",',
+            '"nbdislike": "', proposal_nbdislike, '",',
+            '"nbhate": "', proposal_nbhate, '"',
+            '}'
+        );
+
+        -- Ajout de l'objet à la liste avec gestion de la virgule initiale
+        IF first = 1 THEN
+            SET list_proposal = CONCAT(list_proposal, one_proposal);
+            SET first = 0;
+        ELSE
+            SET list_proposal = CONCAT(list_proposal, ',', one_proposal);
+        END IF;
+    END LOOP;
+
+    CLOSE proposals;
+
+    -- Fermeture de la liste JSON
+    SET list_proposal = CONCAT(list_proposal, ']');
+
+    RETURN list_proposal;
+END //
+
+DELIMITER ;
