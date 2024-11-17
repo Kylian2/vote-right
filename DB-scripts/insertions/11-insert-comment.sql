ba94aed4bace38b612cfdf29ef26b90ce27be536
Esteban <162978612+EstebanDebug@users.noreply.github.com>
2024-11-17 18:55:50 +0100

    fix : harmonisation de la table report avec la nouvelle version de la table comment

--- DB-scripts/insertions/11-insert-comment.sql
+++ DB-scripts/insertions/11-insert-comment.sql
@@ -1,87 +1,53 @@
 INSERT INTO comment (COM_id_NB, COM_message_VC, COM_proposal_NB, COM_sender_NB, COM_suppressor_NB, COM_creation_DATE) VALUES
--- Groupe 1
 (1, 'Cette idée a du potentiel, mais il faut voir le budget.', 1, 25, NULL, '2024-10-31'),
-
 (2, 'La proposition me paraît être une bonne idée, vous en pensez quoi ?', 2, 18, NULL, '2024-11-01'),
 (3, 'Pourquoi pas, c''est la tradition après tout.', 2, 23, NULL, '2024-11-02'),
---(4, 'Faudra que les danseuses portent des tenues légères sinon moi je viens pas.', 2, 25, NULL, '2024-11-02'),
+(4, 'Faudra que les danseuses portent des tenues légères sinon moi je viens pas.', 2, 25, NULL, '2024-11-02'),
 (5, 'Espèce de bouffon, allez hop signalé', 2, 18, NULL, '2024-11-03'),
 (6, 'C''est clair t''a cru qu''on étaient au bordel ou quoi ?', 2, 95, NULL, '2024-11-03'),
-
 (7, 'C''est une bonne idée, ça fait longtemps qu''on n''a pas fait de musée.', 3, 47, NULL, '2024-11-01'),
-
--- Groupe 2
 (8, 'Oui, ça semble aligné avec nos objectifs.', 4, 76, NULL, '2024-11-03'),
-
 (9, 'Excellente idée, trop de personnes sont négligentes.', 6, 80, NULL, '2024-11-03'),
 (10, 'Pas d''objection, c''est pertinent.', 6, 68, NULL, '2024-11-03'),
-
--- Groupe 3
 (11, 'Ouais, c''est une bonne idée la conférence.', 7, 48, NULL, '2024-11-05'),
-
 (12, 'En théorie, l''idée est bonne, mais faudra acheter du matos.', 8, 13, NULL, '2024-11-07'),
-
--- Groupe 4
 (13, 'Certains clubs vont vraiment être surpris par notre niveau j''en suis sûr.', 10, 63, NULL, '2024-11-03'),
---(14, 'Impatient de pouvoir défoncer les abrutis de la ville d''à côté.', 10, 51, 63, '2024-11-06'),
---(15, 'Faut pas les laisser respirer, ces bâtards sont super arrogants.', 10, 51, 63, '2024-11-06'),
+(14, 'Impatient de pouvoir défoncer les abrutis de la ville d''à côté.', 10, 51, 63, '2024-11-06'),
+(15, 'Faut pas les laisser respirer, ces bâtards sont super arrogants.', 10, 51, 63, '2024-11-06'),
 (16, 'C''est pas possible il a recommencer à boire j''en suis sûr.', 10, 93, NULL, '2024-11-06'),
 (17, 'Nan je pense juste qu c''est une personne haineuse, qu''un modérateur fasse quelque chose svp.', 10, 51, NULL, '2024-11-07'),
-
 (18, 'Bonne suggestion, ça semble être un bon complément, je valide l''idée perso.', 11, 51, NULL, '2024-11-08'),
-
---Groupe 5
---(19, 'Les ateliers d''amélioration c''est pour les faibles, vous tous en fait.', 13, 35, 94, '2024-11-14'),
+(19, 'Les ateliers d''amélioration c''est pour les faibles, vous tous en fait.', 13, 35, 94, '2024-11-14'),
 (20, 't''est un géni peut être ? Je te rappel que t''a finit avant dernier à la dernière compétition.', 13, 38, NULL, '2024-11-15'),
 (21, 'Je vais demander à ce qu''on te retire tes droits d''assesseur si tu continu avec ce comportement', 13, 38, NULL, '2024-11-15'),
---(22, 'MDR et la liberté d'expression t'en fait quoi ?? T'est pas modérateur tu sais', 13, 35, 94, '2024-11-14'),
-
---(23, 'N''hésitez pas à me joindre au 0769503412 pour des réductions exclusives sur les canapés poltronesofa.', 14, 38, 94, '2024-11-18'),
+(22, 'MDR et la liberté d''expression t''en fait quoi ?? T''est pas modérateur tu sais', 13, 35, 94, '2024-11-14'),
+(23, 'N''hésitez pas à me joindre au 0769503412 pour des réductions exclusives sur les canapés poltronesofa.', 14, 35, 94, '2024-11-18'),
 (24, 'Ouais nan là tu dégage, on a été beaucoup trop tolérent avec toi', 14, 94, NULL, '2024-11-18'),
-
-
---Groupe 6
 (25, 'Bonne initiative, je pense que tout le monde est d''accord.', 15, 100, NULL, '2024-11-10'),
 (26, 'Oui, c''est une super idée !!!.', 15, 97, NULL, '2024-11-10'),
-
 (27, 'Je pense que c''est une super idée pour les jeunes, mais il faudra bien cadrer.', 16, 100, NULL, '2024-11-11'),
-
 (28, 'Je soutiens la proposition, veillons simplement aux ressources, elles ne sont pas illimitées =).', 17, 89, NULL, '2024-11-01'),
-
---Groupe 7
 (29, 'D''accord pour la proposition, c''est super créatif.', 19, 99, NULL, '2024-11-08'),
-
---Groupe 8
 (30, 'Les élèves ont des lacunes, une remise à niveau leur ferait du bien.', 20, 40, NULL, '2024-11-15'),
-
---Groupe 9
---(31, 'Vous voudriez pas qu''on ouvre un peu plus notre garage à des voitures électriques.', 22, 39, 61, '2024-11-17'),
+(31, 'Vous voudriez pas qu''on ouvre un peu plus notre garage à des voitures électriques.', 22, 39, 61, '2024-11-17'),
 (32, 'Attention à ce que tu raconte gamin tes idéaux de gauchiste tu te les gardent.', 22, 58, NULL, '2024-11-18'),
 (33, 'Ouais faut bien comprendre que le thermique c''est avant tout un état d''esprit.', 22, 61, NULL, '2024-11-18'),
---(34, 'Dommage je trouve que vous êtes un peu borné.', 22, 39, 61, '2024-11-19'),
+(34, 'Dommage je trouve que vous êtes un peu borné.', 22, 39, 61, '2024-11-19'),
 (35, 'Allez hop, par dessus bord, pas d''état d''âme pour les mangeurs de soja', 22, 61, NULL, '2024-11-19'),
 (36, 'Ouais c''est clair, les nouvelles générations c''est vraiement du grand n''importe quoi.', 22, 58, NULL, '2024-11-19'),
-
 (37, 'Il reste des contraintes à résoudre par rapport au parking.', 23, 61, NULL, '2024-11-17'),
 (38, 'Au pire on s''en fout pas besoin d''autorisation, c''est pépouse on présente juste des bagnoles.', 23, 58, NULL, '2024-11-17'),
---(39, 'Les gars, ducoup j''ai acheté une Toyota hybride et franchement ça roule pas mal, je vais l''ammener.', 23, 39, 61, '2024-11-19'),
+(39, 'Les gars, ducoup j''ai acheté une Toyota hybride et franchement ça roule pas mal, je vais l''ammener.', 23, 39, 61, '2024-11-19'),
 (40, 'Et puis quoi encore, garde ta soucoupe volante espèce de boutonneux.', 23, 61, NULL, '2024-11-19'),
-
 (41, 'Bon les gars, qui fait la permanence pour les 2 prochaines semaines.', 23, 58, NULL, '2024-11-20'),
 (42, 'C''est moi qui la faisait jusqu''a maintenant, ça serait bien de faire tourner un peu.', 23, 39, NULL, '2024-11-20'),
 (43, 'Nan c''est très bien comme ça.', 23, 58, NULL, '2024-11-20'),
 (44, 'C''est ta peine pour avoir proféré des insultes à l''encontre du sain moteur diesel.', 23, 58, NULL, '2024-11-20'),
-
---Groupe 10
 (45, 'En tout cas merci à Roger ça fait vraiment plaisir.', 27, 15, NULL, '2024-11-11'),
 (46, 'Oui, c''est une personne avec un cOuaisur grand commme ça.', 27, 22, NULL, '2024-11-12'),
-
 (47, 'Cela fait des années que je garde cette bouteille, elle à une sacré histoire.', 27, 84, NULL, '2024-11-09'),
 (48, 'Pas plus d''un vers par personne. Sinon tout le monde pourra pas expérimenter', 27, 61, NULL, '2024-11-10'),
-
 (49, 'Effectivement, associer le frommage avec le vin, si c''est bien fait, c''est magnifique.', 27, 56, NULL, '2024-11-19'),
-
---Groupe 11
 (50, 'Super initiative, je la soutiens à fond.', 28, 98, NULL, '2024-11-20'),
 (51, 'Ouais, la plage est vraiment sale depuis le dernier festival.', 28, 45, NULL, '2024-11-21'),
 (52, 'Je suis d''accord, faut qu''on fasse quelque chose.', 28, 69, NULL, '2024-11-21'),
@@ -89,80 +55,49 @@ INSERT INTO comment (COM_id_NB, COM_message_VC, COM_proposal_NB, COM_sender_NB, 
 (54, 'Il faut espérer que notre initiative soit suivi, il ne faut pas que la flamme de notre volonté s''éteigne.', 28, 69, NULL, '2024-11-21'),
 (55, 'Il faut garder espoir et être determiner dans notre action', 28, 69, NULL, '2024-11-21'),
 (56, 'T''a raison il ne faut pas se laisser abattre.', 28, 45, NULL, '2024-11-21'),
-
 (57, 'Super comme initiative, faut avant tout sensibiliser la jeunesse.', 29, 98, NULL, '2024-11-22'),
 (58, 'Vraiment la sensibilisation c''est un truc de gauchiste, les amendes c''est plus efficace.', 29, 41, 69, '2024-11-22'),
 (59, 'En fait t''es un déchet, c''est toi qu''on va ramasser sur la plage.', 29, 98, 69, '2024-11-22'),
 (60, 'Vous allez vous calmer tout de suite vous deux, c''est pas sérieux tout ça.', 29, 69, NULL, '2024-11-22'),
-
---Groupe 12
 (61, 'D''accord pour cette idée, mais y a des points à éclaircir.', 31, 17, NULL, '2024-11-23'),
 (62, 'Ouais, c''est sûr qu''il y a encore pas mal de zones d''ombre pour l''instant.', 31, 20, NULL, '2024-11-23'),
 (63, 'Oui, je suis aussi très sceptique.', 31, 86, NULL, '2024-11-23'),
-
 (64, 'C''est une bonne idée qui pourrait augmenter la fréquentation du club.', 32, 97, NULL, '2024-11-23'),
-
---Groupe 13
 (65, 'Il faudrait un artiste assez main stream, qui plaise à la fois aux familles et aux plus jeunes.', 34, 90, NULL, '2024-11-06'),
 (66, 'J''ai entendu parler d''un super groupe, qui pourrait convenir, mais je ne me souviens plus du nom', 34, 92, NULL, '2024-11-06'),
 (67, 'Si tu t''en souviens dit-le nous.', 34, 90, NULL, '2024-11-06'),
 (68, 'Le budget n''est pas non plus énorme il va falloir faire avec.', 34, 90, NULL, '2024-11-06'),
-
-(67, 'Pour les décorations faut aller au plus simple je pense.', 35, 77, NULL, '2024-11-08'),
-(68, 'Oui, je pense qu''on peut reprendre celles de l''année dernière, elles son encore en bon état', 35, 90, NULL, '2024-11-08'),
-(69, 'Moi ça me va, en plus ça fait des économies, l''argent c''est dans nos comptes pas ceux des autres', 35, 12, NULL, '2024-11-08'),
-
-(70, 'C''est une super initiative, pour d''offire un vrai noël aux gens dans le besoin.', 37, 49, NULL, '2024-11-30'),
-(71, 'J''ai tout un placard de jouet de mes enfants qui datent un peu, mais ils sont en très bon état', 37, 19, NULL, '2024-11-30'),
---(72, 'On ne devrait pas aider les gens dans le besoin, ils se sont mis tout seuls dans la merde.', 37, 49, NULL, '2024-11-30');
-(73, 'T''a vraiment pas de cOuaisur, espèce de sale bourgeois, t''est née avec un cuillère en argent dans la bouche', 37, 19, NULL, '2024-11-30'),
---(74, 'Oui, exactement, papa est greffier en tribunal de commerce et maman avocate, et je t'emmerde, sale pauvre', 37, 49, NULL, '2024-11-30');
-
---Groupe 14
-(75, 'C''est une suggestion intéressante, je pense qu''elle mérite qu''on y réfléchisse.', 38, 53, NULL, '2024-12-01'),
-
-(76, 'Je trouve que cette proposition va nous coûter un bras.', 39, 44, NULL, '2024-11-30'),
-
-(77, 'Cette idée pourrait donner de bons résultats, allons-y.', 40, 53, NULL, '2024-12-01'),
-
---Groupe 15
-(78, 'J''ai vraiment pas envie mais ça fait 6 mois que je l''ai pas fait.', 41, 20, NULL, '2024-12-03'),
-(79, 'T''abuses, tu t''es pas dit qu''à un moment faudra que tu la fasses.', 41, 2, NULL, '2024-12-03'),
-(80, 'Ouais, ducoup je me porte volontaire pour ce mois-ci.', 41, 20, NULL, '2024-12-03'),
-
-(81, 'Il nous faut absolument une réponse dans 2 jours.', 43, 6, NULL, '2024-12-02'),
-(82, 'Je suis d''accord avec toi, il faut faire vite.', 43, 27, NULL, '2024-12-03'),
-(83, 'Oui on est sacrément en retard.', 43, 58, NULL, '2024-12-03'),
-
---Groupe 16
-(84, 'Le salon des expositions me paraît être une idée tout à fait adaptée.', 44, 55, NULL, '2024-12-05'),
-
-(85, 'Je suis d''accord, de toute façon on est en retard.', 45, 28, NULL, '2024-12-04'),
-(86, 'Oui, je suis aussi d''accord pour le rassemblement.', 45, 30, NULL, '2024-12-04'),
-
---Groupe 17
-(87, 'L''organisation me paraît correcte.', 46, 59, NULL, '2024-12-05'),
-(88, 'Il faudrait revoir certaines normes de sécurité, mais sinon ok.', 46, 71, NULL, '2024-12-05'),
-
-(89, 'Pour moi, le parcours est bien, ça change de l''année dernière.', 48, 72, NULL, '2024-12-08'),
-
-(90, 'C''est faisable, mais avec une planification rigoureuse.', 49, 65, NULL, '2024-12-10'),
-
-(91, 'Je trouve qu''il n''y a vraiment pas assez de points de ravitaillement.', 50, 72, NULL, '2024-12-09'),
-
---Groupe 18
-(92, 'Je ne suis pas opposé à cette idée.', 51, 67, NULL, '2024-12-11'),
-(93, 'Ouais, c''est super important, vaut mieux prévenir que guérir.', 51, 37, NULL, '2024-12-11'),
-
-(94, 'Je soutiens complètement cette initiative, c''est un beau projet.', 52, 7, NULL, '2024-12-12'),
-(95, 'Ouais, tout pareil.', 52, 46, NULL, '2024-12-12'),
-
-(96, 'Idée prometteuse, on devrait l''étudier de plus près.', 54, 37, NULL, '2024-12-15'), 
-
---Groupe 19
-(97, 'Bonne idée, cette proposition.', 56, 56, NULL, '2024-12-16'),
-(98, 'Ouais, les élèves se font volé souvent.', 56, 57, NULL, '2024-12-16'),
-(99, 'C''est vraiment terrible ces vols ils sont de plus en plus fréquents.', 56, 81, NULL, '2024-12-16'),
-
---Groupe 20
-(100, 'Franchement, j''ai un doute, on doit voter.', 58, 95, NULL, '2024-12-17'),
+(69, 'Pour les décorations faut aller au plus simple je pense.', 35, 77, NULL, '2024-11-08'),
+(70, 'Oui, je pense qu''on peut reprendre celles de l''année dernière, elles son encore en bon état', 35, 90, NULL, '2024-11-08'),
+(71, 'Moi ça me va, en plus ça fait des économies, l''argent c''est dans nos comptes pas ceux des autres', 35, 12, NULL, '2024-11-08'),
+(72, 'C''est une super initiative, pour d''offire un vrai noël aux gens dans le besoin.', 37, 49, NULL, '2024-11-30'),
+(73, 'J''ai tout un placard de jouet de mes enfants qui datent un peu, mais ils sont en très bon état', 37, 19, NULL, '2024-11-30'),
+(74, 'On ne devrait pas aider les gens dans le besoin, ils se sont mis tout seuls dans la merde.', 37, 49, NULL, '2024-11-30'),
+(75, 'T''a vraiment pas de peine, espèce de sale bourgeois, t''est née avec un cuillère en argent dans la bouche', 37, 19, NULL, '2024-11-30'),
+(76, 'Oui, exactement, papa est greffier en tribunal de commerce et maman avocate, et je t''emmerde, sale pauvre', 37, 49, NULL, '2024-11-30'),
+(77, 'C''est une suggestion intéressante, je pense qu''elle mérite qu''on y réfléchisse.', 38, 53, NULL, '2024-12-01'),
+(78, 'Je trouve que cette proposition va nous coûter un bras.', 39, 44, NULL, '2024-11-30'),
+(79, 'Cette idée pourrait donner de bons résultats, allons-y.', 40, 53, NULL, '2024-12-01'),
+(80, 'J''ai vraiment pas envie mais ça fait 6 mois que je l''ai pas fait.', 41, 20, NULL, '2024-12-03'),
+(81, 'T''abuses, tu t''es pas dit qu''à un moment faudra que tu la fasses.', 41, 2, NULL, '2024-12-03'),
+(82, 'Ouais, ducoup je me porte volontaire pour ce mois-ci.', 41, 20, NULL, '2024-12-03'),
+(83, 'Il nous faut absolument une réponse dans 2 jours.', 43, 6, NULL, '2024-12-02'),
+(84, 'Je suis d''accord avec toi, il faut faire vite.', 43, 27, NULL, '2024-12-03'),
+(85, 'Oui on est sacrément en retard.', 43, 58, NULL, '2024-12-03'),
+(86, 'Le salon des expositions me paraît être une idée tout à fait adaptée.', 44, 55, NULL, '2024-12-05'),
+(87, 'Je suis d''accord, de toute façon on est en retard.', 45, 28, NULL, '2024-12-04'),
+(88, 'Oui, je suis aussi d''accord pour le rassemblement.', 45, 30, NULL, '2024-12-04'),
+(89, 'L''organisation me paraît correcte.', 46, 59, NULL, '2024-12-05'),
+(90, 'Il faudrait revoir certaines normes de sécurité, mais sinon ok.', 46, 71, NULL, '2024-12-05'),
+(91, 'Pour moi, le parcours est bien, ça change de l''année dernière.', 48, 72, NULL, '2024-12-08'),
+(92, 'C''est faisable, mais avec une planification rigoureuse.', 49, 65, NULL, '2024-12-10'),
+(93, 'Je trouve qu''il n''y a vraiment pas assez de points de ravitaillement.', 50, 72, NULL, '2024-12-09'),
+(94, 'Je ne suis pas opposé à cette idée.', 51, 67, NULL, '2024-12-11'),
+(95, 'Ouais, c''est super important, vaut mieux prévenir que guérir.', 51, 37, NULL, '2024-12-11'),
+(96, 'Je soutiens complètement cette initiative, c''est un beau projet.', 52, 7, NULL, '2024-12-12'),
+(97, 'Ouais, tout pareil.', 52, 46, NULL, '2024-12-12'),
+(98, 'Idée prometteuse, on devrait l''étudier de plus près.', 54, 37, NULL, '2024-12-15'), 
+(99, 'Bonne idée, cette proposition.', 56, 56, NULL, '2024-12-16'),
+(100, 'Ouais, les élèves se font volé souvent.', 56, 57, NULL, '2024-12-16'),
+(101, 'C''est vraiment terrible ces vols ils sont de plus en plus fréquents.', 56, 81, NULL, '2024-12-16'),
+(102, 'Franchement, j''ai un doute, on doit voter.', 58, 95, NULL, '2024-12-17');
