591077f69e23aec5050afbc8ecded3feb0d00ef3
Kylian2 <kylian.richard@gmail.com>
2024-11-21 14:08:26 +0100

    communities: init
    création de la page et ajout des liens vers la page

---
+++ frontend/pages/communities.vue
@@ -0,0 +1,16 @@
+<template>
+    <Header type="logged" actif="accueil"></Header>
+    <div>
+        <h1>Groupes</h1>
+    </div>
+    <main class="communities">
+
+    </main>
+</template>
+<script setup>
+
+definePageMeta({
+    middleware: ["auth"]
+})
+
+</script>
