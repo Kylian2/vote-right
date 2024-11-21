99380ebd9662ebdd72b56a79a615fb2afe1a7b3d
Kylian2 <kylian.richard@gmail.com>
2024-11-21 14:36:44 +0100

    feat: situation par défaut + modif mineures
    creation de la situation par defaut (quand il n'y a pas de communauté).
    
    modification de style pour rendre globaux ceux qui doivent l'être et rendre locaux ceux qui ne doivent pas l'être.

--- frontend/pages/communities.vue
+++ frontend/pages/communities.vue
@@ -1,10 +1,28 @@
 <template>
     <Header type="logged" actif="accueil"></Header>
-    <div>
+    <div class="communities__title">
         <h1>Groupes</h1>
+        <NuxtLink class="btn--full" to="#">
+            Créer mon groupe
+        </NuxtLink>
     </div>
     <main class="communities">
+        <div class="communities__bloc">
+            <h4>Mes groupes :</h4>
+            <div class="communities__bloc__contener">
+                <div class="add-community" :class="{ 'small': myCommunities.length > 0 }">
+                    <span class="add-community__plus"></span>
+                    <span class="add-community__plus"></span>
+                </div>
+            </div>
+        </div>
 
+        <div class="communities__bloc">
+            <h4>Auxquels je participe :</h4>
+            <div class="communities__bloc__contener">
+                <p v-if="communities.length == 0"> Vous ne participez à aucun groupes </p>
+            </div>
+        </div>
     </main>
 </template>
 <script setup>
@@ -12,5 +30,8 @@
 definePageMeta({
     middleware: ["auth"]
 })
+
+const myCommunities = useState("myCommunities", () => []);
+const communities = useState("communities", () => []);
 
 </script>
