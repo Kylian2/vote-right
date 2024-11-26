87822a93212e6e10926dc0fda2b08f8c3d5cd795
Kylian2 <kylian.richard@gmail.com>
2024-11-26 23:14:12 +0100

    feat: ajout vue role utilisateur

--- DB-scripts/requete-test/test.sql
+++ DB-scripts/requete-test/test.sql
@@ -130,3 +130,11 @@ SELECT COM_id_NB, COM_sender_NB, COM_message_VC, COM_creation_DATE, PRO_communit
 FROM comment C
 INNER JOIN proposal P ON C.COM_proposal_NB = P.PRO_id_NB
 AND P.PRO_id_NB = 2;
+
+-- Affiche les roles de chaque membres au sein des différentes groupes
+CREATE OR REPLACE VIEW members_role AS 
+SELECT  MEM_community_NB, USR_id_NB, USR_firstname_VC, USR_lastname_VC, ROL_label_VC, MEM_role_NB
+FROM user
+INNER JOIN member ON MEM_user_NB = USR_id_NB
+INNER JOIN role ON ROL_id_NB = MEM_role_NB
+ORDER BY MEM_role_NB, MEM_community_NB, USR_firstname_VC, USR_lastname_VC, USR_id_NB;
