40aa81eaff8e4e269403a707857b36a9cf69c803
mguiborat <mathieuguiboratbost@gmail.com>
2024-11-28 15:25:36 +0100

    Fix : changement du trigger insert_vote_round_check

--- DB-scripts/triggers/vote/insert_vote_single_round_check.sql
+++ DB-scripts/triggers/vote/insert_vote_single_round_check.sql
@@ -1,16 +1,24 @@
 DELIMITER //
 
--- Vérifier qu'un vote pour une proposition à un tour ne puisse pas avoir un second tour
-CREATE OR REPLACE TRIGGER insert_vote_single_round_check
+-- Vérifier que le tour du vote ne dépasse pas le nombre maximum de tours du suffrage suivi par ce vote
+CREATE TRIGGER insert_vote_round_check
 BEFORE INSERT ON vote
 FOR EACH ROW
 BEGIN
-    -- Vérifie si le tour est le second tour et si le vote suit un suffrage à un tour
-    IF NEW.VOT_round_NB = 2 AND NEW.VOT_type_NB NOT IN (SELECT SYS_id_NB
-                                                        FROM voting_system
-                                                        WHERE SYS_nb_rounds_NB = 2) THEN
+    DECLARE nb_max_rounds TINYINT;
+
+    -- Récupérer le nombre maximum de tours autorisés pour le système de vote
+    SELECT SYS_nb_rounds_NB INTO nb_max_rounds
+    FROM voting_system
+    WHERE SYS_id_NB = NEW.VOT_type_NB;
+
+    IF NEW.VOT_round_NB > nb_max_rounds THEN
         SIGNAL SQLSTATE '45000'
-        SET MESSAGE_TEXT = 'Erreur : Impossible de créer un deuxième tour pour un suffrage à un tour.';
+        SET MESSAGE_TEXT = CONCAT('Erreur : Impossible de créer un ',
+                                   NEW.VOT_round_NB,
+                                   'ème tour car le vote suit un suffrage à maximum ',
+                                   nb_max_rounds,
+                                   ' tour(s).');
     END IF;
 END //
 
