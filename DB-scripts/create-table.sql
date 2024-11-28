cce84fefdd8b2ea74d55b02c35a7b4d5605e6276
mguiborat <mathieuguiboratbost@gmail.com>
2024-11-28 11:29:06 +0100

    Fix : maj des dates PRO_budget_year_DATE + changement du type de PRO_budget_year_DATE en YEAR et ajout de la contrainte NOT NULL à cet attribut +  ajout d'un trigger qui met par défaut l'année de création de la proposition dans PRO_budget_year_DATE si celui-ci est NULL au moment de l'insertion

--- DB-scripts/create-table.sql
+++ DB-scripts/create-table.sql
@@ -98,7 +98,7 @@ CREATE TABLE proposal (
     PRO_status_VC VARCHAR(20) DEFAULT 'En cours' NOT NULL,
     PRO_initiator_NB INT NOT NULL,
     PRO_deleter_NB INT,
-    PRO_budget_year_DATE DATE,
+    PRO_budget_year_DATE YEAR NOT NULL,
     PRO_approver_NB INT,
     PRO_community_NB INT NOT NULL,
     PRO_theme_NB SMALLINT NOT NULL,
