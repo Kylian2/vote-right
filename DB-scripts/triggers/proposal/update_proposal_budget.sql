e5a7a8bf6388432ee9717cb6de446cf0b35af657
mguiborat <mathieuguiboratbost@gmail.com>
2024-12-01 23:15:25 +0100

    feat: ajout de triggers pour les budgets

--- DB-scripts/triggers/proposal/update_proposal_budget.sql
+++ DB-scripts/triggers/proposal/update_proposal_budget.sql
@@ -5,19 +5,30 @@ CREATE OR REPLACE TRIGGER update_proposal_budget
 BEFORE UPDATE ON proposal
 FOR EACH ROW
 BEGIN
+    DECLARE sumBudgetsProposals INT;
+    DECLARE maximumBudgetTheme INT;
+
     IF NEW.PRO_approver_NB IS NOT NULL
     AND NEW.PRO_budget_NB IS NOT NULL
-    AND (OLD.PRO_budget_NB IS NULL OR NEW.PRO_budget_NB > OLD.PRO_budget_NB)
-    AND NEW.PRO_budget_NB + (SELECT COALESCE(SUM(PRO_budget_NB), 0)
-                             FROM proposal
-                             WHERE PRO_id_NB != NEW.PRO_id_NB
-                             AND PRO_theme_NB = NEW.PRO_theme_NB
-                             AND PRO_community_NB = NEW.PRO_community_NB) > (SELECT THM_budget_NB
-                                                                             FROM theme 
-                                                                             WHERE THM_id_NB = NEW.PRO_theme_NB)
-    THEN
-        SIGNAL SQLSTATE '45000'
-        SET MESSAGE_TEXT = 'Erreur : Le budget de cette proposition fait dépasser le budget total alloué pour son thème.';
+    AND (OLD.PRO_budget_NB IS NULL OR NEW.PRO_budget_NB > OLD.PRO_budget_NB) THEN
+
+        SELECT COALESCE(SUM(PRO_budget_NB), 0) + NEW.PRO_budget_NB INTO sumBudgetsProposals
+        FROM proposal
+        WHERE PRO_id_NB != NEW.PRO_id_NB
+        AND PRO_theme_NB = NEW.PRO_theme_NB
+        AND PRO_community_NB = NEW.PRO_community_NB
+        AND PRO_period_YEAR = NEW.PRO_period_YEAR;
+
+        SELECT BUT_amount_NB INTO maximumBudgetTheme
+        FROM theme_budget
+        WHERE BUT_community_NB = NEW.PRO_community_NB
+        AND BUT_theme_NB = NEW.PRO_theme_NB
+        AND BUT_period_YEAR = NEW.PRO_period_YEAR;
+
+        IF sumBudgetsProposals > maximumBudgetTheme THEN
+            SIGNAL SQLSTATE '45000'
+            SET MESSAGE_TEXT = 'Erreur : Le budget de cette proposition fait dépasser le budget total alloué pour son thème.';
+        END IF;
     END IF;
 END //
 
