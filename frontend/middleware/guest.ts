887790c8452491fc964f6d54ba9291b026be6a77
Kylian2 <kylian.richard@gmail.com>
2024-12-08 10:41:56 +0100

    debug: middleware

--- frontend/middleware/guest.ts
+++ frontend/middleware/guest.ts
@@ -1,9 +1,14 @@
-export default defineNuxtRouteMiddleware(async () => {
+export default defineNuxtRouteMiddleware(async (to, from) => {
     const config = useRuntimeConfig() //Pour utiliser les variables d'environnement, sera utile lorsque l'on mettra les routes de productions
     try {
         const headers = useRequestHeaders(['cookie']);
         const response = await $fetch(`${config.public.baseUrl}/auth/check`, { headers, credentials : 'include'});
-        console.log("GUEST AUTH MIDDLEWARE");
+        console.log("FROM GUEST MIDDLEWARE --- ");
+        console.log(to.fullPath);
+        console.log(from.fullPath);
+        console.log(import.meta.client ? "client" : "serveur");
+        console.log(response);
+        console.log (" ------- ");
         if (response === true) { 
             return navigateTo('/home');
         }
