fd7692b0b2e4683ed5a3c92efaf07a686d69a105
Kylian2 <kylian.richard@gmail.com>
2024-12-14 14:25:08 +0100

    fix: middleware

--- frontend/middleware/communityMember.ts
+++ frontend/middleware/communityMember.ts
@@ -1,6 +1,8 @@
 export default defineNuxtRouteMiddleware(async (to, from) => {
     const config = useRuntimeConfig() //Pour utiliser les variables d'environnement, sera utile lorsque l'on mettra les routes de productions
-
+    console.log(to);
+    console.log(from);
+    
     if(import.meta.server){
         return;
     }
@@ -8,8 +10,7 @@ export default defineNuxtRouteMiddleware(async (to, from) => {
     try {
         const headers = useRequestHeaders(['cookie']);
         const response = await $fetch(`${config.public.baseUrl}/communities/${to.params.id}/membership`, { headers, credentials : 'include'});
-        console.log(to);
-        console.log(from);
+
         if (response !== true) { 
             return abortNavigation();
         }
--- frontend/middleware/proposalAccess.ts
+++ frontend/middleware/proposalAccess.ts
@@ -1,6 +1,9 @@
 export default defineNuxtRouteMiddleware(async (to, from) => {
     const config = useRuntimeConfig() //Pour utiliser les variables d'environnement, sera utile lorsque l'on mettra les routes de productions
     
+    console.log(to);
+    console.log(from);
+    
     if(import.meta.server){
         return;
     }
@@ -8,8 +11,7 @@ export default defineNuxtRouteMiddleware(async (to, from) => {
     try {
         const headers = useRequestHeaders(['cookie']);
         const response = await $fetch(`${config.public.baseUrl}/proposals/${to.params.id}/membership`, { headers, credentials : 'include'});
-        console.log(to);
-        console.log(from);
+ 
         if (response !== true) { 
             return abortNavigation();
         }
