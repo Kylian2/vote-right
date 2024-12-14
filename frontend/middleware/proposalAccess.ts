c7133f06e6809f6e95f0eeac5faea6ae542badb0
Kylian2 <kylian.richard@gmail.com>
2024-12-14 16:42:50 +0100

    fix: middleware

--- frontend/middleware/proposalAccess.ts
+++ frontend/middleware/proposalAccess.ts
@@ -1,14 +1,11 @@
 export default defineNuxtRouteMiddleware(async (to, from) => {
     const config = useRuntimeConfig() //Pour utiliser les variables d'environnement, sera utile lorsque l'on mettra les routes de productions
 
-    if(import.meta.server){
-        return abortNavigation();
-    }
     try {
         const headers = useRequestHeaders(['cookie']);
+        console.log(headers)
         const response = await $fetch(`${config.public.baseUrl}/proposals/${to.params.id}/membership`, { headers, credentials : 'include'});
-        console.log(to);
-        console.log(from);
+
         if (response !== true) { 
             return navigateTo('/home');
         }
