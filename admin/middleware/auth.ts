ec284ea21d51dbaeb8e73ea348e521c534ce6d16
Kylian2 <kylian.richard@gmail.com>
2024-12-23 16:38:21 +0100

    feat: page de connexion

---
+++ admin/middleware/auth.ts
@@ -0,0 +1,16 @@
+export default defineNuxtRouteMiddleware(async (to, from) => {
+    const config = useRuntimeConfig() //Pour utiliser les variables d'environnement, sera utile lorsque l'on mettra les routes de productions
+    if(import.meta.server && to.fullPath === from.fullPath){
+        return
+    }
+    try {
+        const headers = useRequestHeaders(['cookie']);
+        const response = await $fetch(`${config.public.baseUrl}/auth/check`, { headers, credentials : 'include'});
+        if (response !== true) { 
+            return navigateTo('/login');
+        }
+    } catch (error) {
+        console.error("Error during authorization check:", error);
+        return abortNavigation();
+    }
+})
