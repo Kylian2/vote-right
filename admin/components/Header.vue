add25bbaba023b6394e35086fa7293f4598d8052
Kylian2 <kylian.richard@gmail.com>
2024-12-23 17:22:05 +0100

    feat: liste des groupes administré

---
+++ admin/components/Header.vue
@@ -0,0 +1,12 @@
+<template>
+
+    <header class="application-header">
+        <p class="desktop">VoteRightControlPanel</p>
+        <p class="mobile">VRCP</p>
+        <div>
+            <Nuxtlink class="btn btn--small" to="/home" >Accueil</Nuxtlink>
+            <Nuxtlink class="btn btn--small" to="/" >Déconnexion</Nuxtlink>
+        </div>
+    </header>
+
+</template>
--- admin/pages/home.vue
+++ admin/pages/home.vue
@@ -1,5 +1,45 @@
 <template>
+<Header></Header>
+<h1>Groupes gérés</h1>
 
-<h1>Home</h1>
+<main class="home">
+
+    <div class="community-list">
+        <p v-for="community, key in communities" 
+            @mouseover="(event) => event.target.style.color = community['CMY_color_VC']" 
+            @mouseout="(event) => event.target.style.color = ''">
+        <strong>{{community["CMY_name_VC"]}}</strong> <span>{{community["CMY_nb_member_NB"] >= 2 ? community["CMY_nb_member_NB"] + " membres" : "1 membre"}}</span></p>
+    </div>
+
+</main>
 
 </template>
+<script setup>
+
+definePageMeta({
+  middleware: ["auth"]
+})
+
+const config = useRuntimeConfig();
+
+const communities = ref();
+
+const fetchData =  async() =>{
+
+    try{
+        const response = await $fetch(`${config.public.baseUrl}/communities/administered`, {
+            credentials: 'include',
+        });
+
+        communities.value = response
+    } catch (error) {
+        console.log("An error occured", error);
+    }
+
+}
+
+onMounted(() => {
+    fetchData();
+})
+
+</script>
