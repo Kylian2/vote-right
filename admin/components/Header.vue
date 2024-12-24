37af17bb5f159786eebaa87e39391d776d13b67b
Kylian2 <kylian.richard@gmail.com>
2024-12-24 11:46:39 +0100

    feat: bouton deconnexion

--- admin/components/Header.vue
+++ admin/components/Header.vue
@@ -5,8 +5,15 @@
         <p class="mobile">VRCP</p>
         <div>
             <NuxtLink class="btn btn--small" to="/home" >Accueil</NuxtLink>
-            <NuxtLink class="btn btn--small" to="/" >Déconnexion</NuxtLink>
+            <NuxtLink class="btn btn--small" to="/" @click="disconnect()">Déconnexion</NuxtLink>
         </div>
     </header>
 
 </template>
+<script setup>
+
+const disconnect = () => {
+    document.cookie = 'PHPSESSID=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
+}
+
+</script>
