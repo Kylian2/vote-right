caf4d7309bddf7e11ac417b52d28973452cc4d58
Kylian <86042198+Kylian2@users.noreply.github.com>
2024-12-31 18:12:01 +0100

    Delete .spectral.mjs

--- .spectral.mjs
+++
@@ -1,2 +0,0 @@
-import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjI2ODI3MQ/spectral.js?branch_slug=main&token=2aba2a85-6adf-438c-a1d5-f8b8c46999b9";
-export default { extends: ruleset };
