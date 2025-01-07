48e9abbc0dd8aff4a0398542b11ecd95c19957ca
Kylian2 <kylian.richard@gmail.com>
2025-01-07 12:44:46 +0100

    feat: meta title

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -21,4 +21,11 @@ export default defineNuxtConfig({
     },
   },
   ssr: false,
+  app: {
+    head: {
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+      title: 'VoteRight'
+    }
+  }
 })
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -5,4 +5,11 @@
 definePageMeta({
   middleware: ["guest"]
 })
+
+useHead({
+  title: 'VoteRight',
+  meta: [
+    { name: 'description', content: 'VoteRight : donnez à votre communauté le pouvoir de proposer, voter, discuter et gérer des projets collectifs. Idéal pour collectivités et associations engagées !' }
+  ],
+})
 </script>
