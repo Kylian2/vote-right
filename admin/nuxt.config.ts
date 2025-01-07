3a423e70aa9569693ed58275af5722ee8cbaf719
Kylian2 <kylian.richard@gmail.com>
2025-01-07 12:45:21 +0100

    feat: meta title

--- admin/nuxt.config.ts
+++ admin/nuxt.config.ts
@@ -21,4 +21,11 @@ export default defineNuxtConfig({
       },
     },
   },
+  app: {
+    head: {
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+      title: 'Admin | VoteRight'
+    }
+  }
 })
