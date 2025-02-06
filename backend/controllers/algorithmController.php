38afd990088ec4c6e4ef2d80ec755d2a23f06906
Kylian2 <kylian.richard@gmail.com>
2025-02-06 19:30:07 +0100

    feat: liaison api algorithm

---
+++ backend/controllers/algorithmController.php
@@ -0,0 +1,66 @@
+<?php
+
+class algorithmController{
+    
+    /**
+     * REQUETE À API ALGORITHM
+     * Lance l'algorithme de recommandation pour la communauté passé via l'url pour la periode donnée.
+     * 
+     * $_GET['period'] : la periode de traitement
+     * 
+     * @param array $params : $params[0] l'id de la communauté
+     */
+    public static function proposal(array $params){
+
+        if(!isset($_GET['period']) || !is_numeric($_GET['period'])){
+            http_response_code(400);
+            echo json_encode(array('error' => 'period is incorrect or not found'));
+            return;
+        }
+
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+        CURLOPT_URL => $_ENV['ALGO_API'].'/proposals?community='.$params[0].'&period='.$_GET['period'],
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'GET',
+        CURLOPT_HTTPHEADER => array(
+            'X-API-KEY: '.$_ENV['KEY']
+        ),
+        ));
+
+        $response = curl_exec($curl);
+
+        if(curl_errno($curl)){
+            http_response_code(500);
+            echo 'Erreur cURL :'.curl_error($curl);
+        }
+        curl_close($curl);
+
+        echo $response;
+    }
+
+    /**
+     * Permet de tester la connexion à l'api algorithms
+     */
+    public static function testApi(){
+        //paramètre à ajuster en fonction de l'environnement
+        $host = 'host.docker.internal:3334';
+        $port = 3334;
+        $timeout = 2; // 2 secondes
+        
+        $connection = @fsockopen($host, $port, $errno, $errstr, $timeout);
+        if ($connection) {
+            echo "Connexion réussie à $host:$port";
+            fclose($connection);
+        } else {
+            echo "Erreur de connexion à $host:$port - $errstr ($errno)";
+        }
+    }
+
+}
--- backend/route/routes.php
+++ backend/route/routes.php
@@ -29,6 +29,7 @@ Router::get('/communities/{id}/periods', 'communityController@periods', true);
 Router::get('/communities/{id}/proposals/formatted', 'communityController@formattedProposals', true);
 Router::get('/communities/{id}/reports', 'reportController@index', true);
 Router::patch('/communities/{id}/update', 'communityController@update');
+Router::get('/communities/{id}/algo', 'algorithmController@proposal', true);
 
 Router::get('/users', 'userController@index');
 Router::get('/users/{id}', 'userController@show');
