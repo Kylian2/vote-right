21a053ab799164a9ca79eb3e3e58da219b3424fc
mguiborat <mathieuguiboratbost@gmail.com>
2025-06-01 23:58:14 +0200

    Feat : ajout des tris à la page propositions

--- frontend/pages/community/[id]/proposals.vue
+++ frontend/pages/community/[id]/proposals.vue
@@ -3,19 +3,26 @@
     <Banner :community="community" :themes="communityThemes" back>{{ community["CMY_name_VC"] }}</Banner>
     <main class="community-proposals">
         <div class="filter">
-            <p @click="updateFilter" @mouseover="hover = true" @mouseleave="hover = false"
-                :style="{ color: hover ? community['CMY_color_VC'] : 'inherit' }">
-                {{ hideFilter ? 'Filtrer' : 'Masquer' }}</p>
-            <img src="/public/images/icons/grid-active.png" alt="Vue grille" v-if="view === 'grid'"
-                @click="view = 'grid'"/>
-            <img src="/public/images/icons/grid-inactive.png" alt="Vue grille inactive" v-else
-                @click="view = 'grid'"/>
-            <img src="/public/images/icons/list-active.png" alt="Vue liste" v-if="view === 'list'"
-                @click="view = 'list'"/>
-            <img src="/public/images/icons/list-inactive.png" alt="Vue liste inactive" v-else
-                @click="view = 'list'"/>
+            <p @click="updateSort" :style="{ color: hoverSort ? community['CMY_color_VC'] : 'inherit' }">Trier</p>
+            <p @click="updateFilter" :style="{ color: hoverFilter ? community['CMY_color_VC'] : 'inherit' }">Filtrer</p>
+            <img v-if="view === 'grid'" src="/public/images/icons/grid-active.png" alt="Vue grille"/>
+            <img v-else @click="view = 'grid'" src="/public/images/icons/grid-inactive.png" alt="Vue grille inactive"/>
+            <img v-if="view === 'list'" src="/public/images/icons/list-active.png" alt="Vue liste"/>
+            <img v-else @click="view = 'list'" src="/public/images/icons/list-inactive.png" alt="Vue liste inactive"/>
         </div>
-        <div v-if="!hideFilter" class="filter-container">
+        <div v-if="showFilter && typeFilter == 'Trier'" class="filter-container">
+            <div class="filter-container__block">
+                <img src="/public/images/icons/grid-active.png" alt="Date chronologique"
+                @click="sortProposals('Date chronologique')"/>
+                <img src="/public/images/icons/grid-active.png" alt="Date antéchronologique"
+                @click="sortProposals('Date antéchronologique')"/>
+                <img src="/public/images/icons/grid-active.png" alt="Budget croissant"
+                @click="sortProposals('Budget croissant')"/>
+                <img src="/public/images/icons/grid-active.png" alt="Budget décroissant"
+                @click="sortProposals('Budget décroissant')"/>
+            </div>
+        </div>
+        <div v-if="showFilter && typeFilter == 'Filtrer'" class="filter-container">
             <div class="filter-container__block">
                 <select v-model="filter" name="theme" @change="updateFilteredProposals">
                     <option value="">Filtrer</option>
@@ -88,10 +95,12 @@ const selectedProposals = ref();
 const filter = ref('');
 const statuses = ["Validée", "Rejetée", "En cours"];
 const checkedStatus = ref([]);
-const hideFilter = ref(true);
-const hover = ref(false);
+const showFilter = ref(false);
+const hoverSort = ref(false);
+const hoverFilter = ref(false);
 const view = ref('list');
 const isDataFetched = ref(false);
+const typeFilter = ref('');
 
 const updateFilteredProposals = () => {
     let filtered = proposals.value;
@@ -107,14 +116,149 @@ const updateFilteredProposals = () => {
     selectedProposals.value = filtered;
 }
 
-const updateFilter = () => {
-    hideFilter.value = !hideFilter.value;
-
-    if(hideFilter.value == true){
-        checkedStatus.value = [];
-        filter.value = '';
-        updateFilteredProposals();
+const updateSort = () => {
+    if (typeFilter.value == 'Trier') {
+        typeFilter.value = '';
+        hoverSort.value = false;
+        showFilter.value = false;
+    } else {
+        typeFilter.value = 'Trier';
+        hoverSort.value = true;
+        hoverFilter.value = false;
+        showFilter.value = true;
     }
+}
+
+const updateFilter = () => {
+    if (typeFilter.value == 'Filtrer') {
+        typeFilter.value = '';
+        hoverFilter.value = false;
+        showFilter.value = false;
+    } else {
+        typeFilter.value = 'Filtrer';
+        hoverFilter.value = true;
+        hoverSort.value = false;
+        showFilter.value = true;
+    }
+}
+
+const sortProposals = (choice) => {
+    if (choice == "Date chronologique") {
+        selectedProposals.value = mergeSortProposalsByDate(selectedProposals.value);
+    }
+    if (choice == "Date antéchronologique") {
+        selectedProposals.value = mergeSortProposalsByDateDesc(selectedProposals.value);
+    }
+    if (choice == "Budget croissant") {
+        selectedProposals.value = mergeSortProposals(selectedProposals.value);
+    }
+    if (choice == "Budget décroissant") {
+        selectedProposals.value = mergeSortProposalsDesc(selectedProposals.value);
+    }
+}
+
+const mergeSortProposalsByDate = (proposals) => {
+    if (proposals.length <= 1) {
+        return proposals;
+    }
+
+    let middle = Math.floor(proposals.length / 2);
+    let left = mergeSortProposalsByDate(proposals.slice(0, middle));
+    let right = mergeSortProposalsByDate(proposals.slice(middle));
+
+    return mergeByDate(left, right);
+}
+
+const mergeByDate = (left, right) => {
+    let result = [];
+
+    while (left.length && right.length) {
+        if (left[0]['PRO_period_YEAR'] < right[0]['PRO_period_YEAR']) {
+            result.push(left.shift());
+        } else {
+            result.push(right.shift());
+        }
+    }
+
+    return result.concat(left, right);
+}
+
+const mergeSortProposalsByDateDesc = (proposals) => {
+    if (proposals.length <= 1) {
+        return proposals;
+    }
+
+    let middle = Math.floor(proposals.length / 2);
+    let left = mergeSortProposalsByDateDesc(proposals.slice(0, middle));
+    let right = mergeSortProposalsByDateDesc(proposals.slice(middle));
+
+    return mergeByDateDesc(left, right);
+}
+
+const mergeByDateDesc = (left, right) => {
+    let result = [];
+
+    while (left.length && right.length) {
+        if (left[0]['PRO_period_YEAR'] > right[0]['PRO_period_YEAR']) {
+            result.push(left.shift());
+        } else {
+            result.push(right.shift());
+        }
+    }
+
+    return result.concat(left, right);
+}
+
+const mergeSortProposals = (proposals) => {
+    if (proposals.length <= 1) {
+        return proposals;
+    }
+
+    let middle = Math.floor(proposals.length / 2);
+    let left = mergeSortProposals(proposals.slice(0, middle));
+    let right = mergeSortProposals(proposals.slice(middle));
+
+    return merge(left, right);
+}
+
+const mergeSortProposalsDesc = (proposals) => {
+    if (proposals.length <= 1) {
+        return proposals;
+    }
+
+    let middle = Math.floor(proposals.length / 2);
+    let left = mergeSortProposalsDesc(proposals.slice(0, middle));
+    let right = mergeSortProposalsDesc(proposals.slice(middle));
+
+    return mergeDesc(left, right);
+}
+
+const merge = (left, right) => {
+    let result = [];
+
+    while (left.length && right.length) {
+        if (left[0]['PRO_budget_NB'] < right[0]['PRO_budget_NB']) {
+            result.push(left.shift());
+        } else {
+            result.push(right.shift());
+        }
+    }
+
+    return result.concat(left, right);
+}
+
+const mergeDesc = (left, right) => {
+    let result = [];
+
+    while (left.length && right.length) {
+        if (left[0]['PRO_budget_NB'] > right[0]['PRO_budget_NB']) {
+            result.push(left.shift());
+        } else {
+            result.push(right.shift());
+        }
+    }
+
+    return result.concat(left, right);
 }
 
 const fetchData = async () => {
